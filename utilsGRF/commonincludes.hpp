1c124cc16b29fc7e4ff17cbacca01ccb2a50feb0
Rosa Martinez Corral <rosamcorral@hotmail.com>
2021-12-13 12:26:44 +0100

    A few common include directives.

---
+++ utilsGRF/commonincludes.hpp
@@ -0,0 +1,14 @@
+#include <boost/multiprecision/mpfr.hpp>
+#include <boost/multiprecision/mpc.hpp>
+#include "GRFCalculations.hpp"
+#include <polynomial.hpp>
+
+
+
+using boost::multiprecision::number; 
+using boost::multiprecision::mpfr_float_backend; 
+using boost::multiprecision::mpc_complex_backend; 
+
+
+
+//https://stackoverflow.com/questions/47487888/pybind11-template-class-of-many-types/47749076
